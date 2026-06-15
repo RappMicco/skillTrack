38bd294cdf79e79684180e769085a2d0450314fd
RAPP MICCO RIZO <rapp.miccorizo@gmail.com>
2026-06-15 13:29:10 +0000

    Server

--- server/routes/employeeRoutes.js
+++ server/routes/employeeRoutes.js
@@ -1,8 +1,13 @@
 import express from "express";
-import { checkEmployeeId, registerEmployee } from "../controllers/auth.js";
+import {
+  checkEmployeeId,
+  registerEmployee,
+  updateEmployeeData,
+} from "../controllers/auth.js";
 import {
   validateEmployeeId,
   validateRegistration,
+  validateUpdateEmployee,
 } from "../validators/employeeValidator.js";
 import { validateResult } from "../middlewares/validateRequest.js";
 import { protect } from "../middlewares/protectMiddleware.js";
@@ -18,6 +23,14 @@ router.post(
   validateRegistration,
   validateResult,
   registerEmployee,
+);
+router.put(
+  "/update/:id",
+  isAuthenticated,
+  protect,
+  validateUpdateEmployee,
+  validateResult,
+  updateEmployeeData,
 );
 
 export default router;
