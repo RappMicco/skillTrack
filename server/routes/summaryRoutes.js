86b9f4a4ef49f2b8011728fe977a0610a73c34d4
RappMicco <rapp.miccorizo@gmail.com>
2026-06-17 17:08:10 +0800

    feat: done summary controller

--- server/routes/summaryRoutes.js
+++ server/routes/summaryRoutes.js
@@ -1,11 +1,23 @@
 import express from "express";
 import { getTrainingStatusSummary } from "../controllers/getTrainingStatusController.js";
-import { employeesSummary } from "../controllers/getMaintenanceSummary.js";
+import {
+  employeesSummary,
+  assignedTrainingSummary,
+  skillCategorySummary,
+  skillMatrixSummary,
+  skillProficiencySummary,
+  trainingProviderSummary,
+} from "../controllers/getMaintenanceSummary.js";
 import { isAuthenticated } from "../middlewares/authMiddleware.js";
 
 const router = express.Router();
 
 router.get("/get-training-summary", isAuthenticated, getTrainingStatusSummary);
 router.get("/get-employees", isAuthenticated, employeesSummary);
+router.get("/get-assigned-training", isAuthenticated, assignedTrainingSummary);
+router.get("/get-skill-category", isAuthenticated, skillCategorySummary);
+router.get("/get-skill-matrix", isAuthenticated, skillMatrixSummary);
+router.get("/get-skill-proficiency", isAuthenticated, skillProficiencySummary);
+router.get("/get-training-provider", isAuthenticated, trainingProviderSummary);
 
 export default router;
