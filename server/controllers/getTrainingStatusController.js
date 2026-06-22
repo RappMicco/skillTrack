886f3b2e9d97f5d7877515ce3a580c6dd1dc2b3d
RappMicco <rapp.miccorizo@gmail.com>
2026-06-22 15:15:50 +0800

    feat: add all status data

--- server/controllers/getTrainingStatusController.js
+++ server/controllers/getTrainingStatusController.js
@@ -1,5 +1,7 @@
 import mongoose from "mongoose";
 import { EmployeeTraining } from "../models/employeeTrainingModel.js";
+import { Training } from "../models/trainingModel.js";
+import { Employee } from "../models/employeesModel.js";
 
 export const getTrainingStatusSummary = async (req, res) => {
   try {
@@ -68,6 +70,402 @@ export const getTrainingStatusSummary = async (req, res) => {
     return res.status(500).json({
       success: false,
       message: "Server error during fetching of all training status!",
+      error: error.message,
+    });
+  }
+};
+
+export const upcomingSummary = async (req, res) => {
+  try {
+    const summary = await EmployeeTraining.aggregate([
+      {
+        $lookup: {
+          from: "employees",
+          localField: "empId",
+          foreignField: "_id",
+          as: "employee",
+        },
+      },
+      {
+        $unwind: {
+          path: "$employee",
+          preserveNullAndEmptyArrays: true,
+        },
+      },
+      {
+        $lookup: {
+          from: "trainings",
+          localField: "trainingId",
+          foreignField: "_id",
+          as: "training",
+        },
+      },
+      {
+        $unwind: {
+          path: "$training",
+          preserveNullAndEmptyArrays: true,
+        },
+      },
+      {
+        $lookup: {
+          from: "status",
+          localField: "statusId",
+          foreignField: "_id",
+          as: "status",
+        },
+      },
+      {
+        $unwind: {
+          path: "$status",
+          preserveNullAndEmptyArrays: true,
+        },
+      },
+      {
+        $match: {
+          "employee.isActive": true,
+          "status.status": "upcoming",
+        },
+      },
+      {
+        $project: {
+          empId: "$employee.empId",
+          fullName: {
+            $trim: {
+              input: {
+                $concat: [
+                  { $ifNull: ["$employee.firstName", ""] },
+                  " ",
+                  { $ifNull: ["$employee.lastName", ""] },
+                ],
+              },
+            },
+          },
+          group: "$employee.group",
+          empLevel: "$employee.empLevel",
+          trainingName: "$training.traingName",
+          trainingProvider: "$training.trainingProvider",
+          status: "$status.status",
+          startDate: 1,
+          endDate: 1,
+          progress: 1,
+          remarks: 1,
+        },
+      },
+    ]);
+
+    if (summary.length === 0) {
+      return res.status(200).json({
+        success: true,
+        message: "No upcoming training!",
+      });
+    }
+
+    res.status(200).json({
+      success: true,
+      data: summary,
+    });
+  } catch (error) {
+    console.error("Fetching error: ", error);
+    return res.status(500).json({
+      success: false,
+      message: "Server error during fetching of upcoming status data!",
+      error: error.message,
+    });
+  }
+};
+
+export const pendingSummary = async (req, res) => {
+  try {
+    const summary = await EmployeeTraining.aggregate([
+      {
+        $lookup: {
+          from: "employees",
+          localField: "empId",
+          foreignField: "_id",
+          as: "employee",
+        },
+      },
+      {
+        $unwind: {
+          path: "$employee",
+          preserveNullAndEmptyArrays: true,
+        },
+      },
+      {
+        $lookup: {
+          from: "trainings",
+          localField: "trainingId",
+          foreignField: "_id",
+          as: "training",
+        },
+      },
+      {
+        $unwind: {
+          path: "$training",
+          preserveNullAndEmptyArrays: true,
+        },
+      },
+      {
+        $lookup: {
+          from: "status",
+          localField: "statusId",
+          foreignField: "_id",
+          as: "status",
+        },
+      },
+      {
+        $unwind: {
+          path: "$status",
+          preserveNullAndEmptyArrays: true,
+        },
+      },
+      {
+        $match: {
+          "employee.isActive": true,
+          "status.status": "pending",
+        },
+      },
+      {
+        $project: {
+          empId: "$employee.empId",
+          fullName: {
+            $trim: {
+              input: {
+                $concat: [
+                  { $ifNull: ["$employee.firstName", ""] },
+                  " ",
+                  { $ifNull: ["$employee.lastName", ""] },
+                ],
+              },
+            },
+          },
+          group: "$employee.group",
+          empLevel: "$employee.empLevel",
+          trainingName: "$training.trainingName",
+          trainingProvider: "$training.trainingProvider",
+          status: "$status.status",
+          startDate: 1,
+          endDate: 1,
+          progress: 1,
+          remarks: 1,
+        },
+      },
+    ]);
+
+    if (summary.length === 0) {
+      return res.status(200).json({
+        success: true,
+        message: "No pending training!",
+      });
+    }
+
+    res.status(200).json({
+      success: true,
+      data: summary,
+    });
+  } catch (error) {
+    console.error("Fetching error: ", error);
+    return res.status(500).json({
+      success: false,
+      message: "Server error during fetching of pending status data!",
+      error: error.message,
+    });
+  }
+};
+
+export const ongoingSummary = async (req, res) => {
+  try {
+    const summary = await EmployeeTraining.aggregate([
+      {
+        $lookup: {
+          from: "employees",
+          localField: "empId",
+          foreignField: "_id",
+          as: "employee",
+        },
+      },
+      {
+        $unwind: {
+          path: "$employee",
+          preserveNullAndEmptyArrays: true,
+        },
+      },
+      {
+        $lookup: {
+          from: "trainings",
+          localField: "trainingId",
+          foreignField: "_id",
+          as: "training",
+        },
+      },
+      {
+        $unwind: {
+          path: "$training",
+          preserveNullAndEmptyArrays: true,
+        },
+      },
+      {
+        $lookup: {
+          from: "status",
+          localField: "statusId",
+          foreignField: "_id",
+          as: "status",
+        },
+      },
+      {
+        $unwind: {
+          path: "$status",
+          preserveNullAndEmptyArrays: true,
+        },
+      },
+      {
+        $match: {
+          "employee.isActive": true,
+          "status.status": "ongoing",
+        },
+      },
+      {
+        $project: {
+          empId: "$employee.empId",
+          fullName: {
+            $trim: {
+              input: {
+                $concat: [
+                  { $ifNull: ["$employee.firstName", ""] },
+                  " ",
+                  { $ifNull: ["$employee.lastName", ""] },
+                ],
+              },
+            },
+          },
+          empLevel: "$employee.empLevel",
+          group: "$employee.group",
+          trainingName: "$training.trainingName",
+          trainingProvider: "$training.trainingProvider",
+          status: "$status.status",
+          startDate: 1,
+          endDate: 1,
+          progress: 1,
+          remarks: 1,
+        },
+      },
+    ]);
+
+    if (summary.length === 0) {
+      return res.status(500).json({
+        success: true,
+        message: "No ongoing training!",
+      });
+    }
+
+    res.status(500).json({
+      success: true,
+      data: summary,
+    });
+  } catch (error) {
+    console.error("Fetching error: ", error);
+    return res.status(500).json({
+      success: false,
+      message: "Server error during fetching of ongoing statud data!",
+      error: error.message,
+    });
+  }
+};
+
+export const completedSummary = async (req, res) => {
+  try {
+    const summary = await EmployeeTraining.aggregate([
+      {
+        $lookup: {
+          from: "employees",
+          localField: "empId",
+          foreignField: "_id",
+          as: "employee",
+        },
+      },
+      {
+        $unwind: {
+          path: "$employee",
+          preserveNullAndEmptyArrays: true,
+        },
+      },
+      {
+        $lookup: {
+          from: "trainings",
+          localField: "trainingId",
+          foreignField: "_id",
+          as: "training",
+        },
+      },
+      {
+        $unwind: {
+          path: "$training",
+          preserveNullAndEmptyArrays: true,
+        },
+      },
+      {
+        $lookup: {
+          from: "status",
+          localField: "statusId",
+          foreignField: "_id",
+          as: "status",
+        },
+      },
+      {
+        $unwind: {
+          path: "$status",
+          preserveNullAndEmptyArrays: true,
+        },
+      },
+      {
+        $match: {
+          "employee.isActive": true,
+          "status.status": "completed",
+        },
+      },
+      {
+        $project: {
+          empId: "$employee.empId",
+          fullName: {
+            $trim: {
+              input: {
+                $concat: [
+                  { $ifNull: ["$employee.firstName", ""] },
+                  " ",
+                  { $ifNull: ["$employee.lastName", ""] },
+                ],
+              },
+            },
+          },
+          empLevel: "$employee.empLevel",
+          group: "$employee.group",
+          trainingName: "$training.trainingName",
+          trainingProvider: "$training.trainingProvider",
+          status: "$status.status",
+          startDate: 1,
+          endDate: 1,
+          progress: 1,
+          remarks: 1,
+        },
+      },
+    ]);
+
+    if (summary.length === 0) {
+      return res.status(500).json({
+        success: true,
+        message: "No completed training!",
+      });
+    }
+
+    res.status(500).json({
+      success: true,
+      data: summary,
+    });
+  } catch (error) {
+    console.error("Fetching error: ", error);
+    return res.status(500).json({
+      success: false,
+      message: "Server error during fetching of ongoing statud data!",
       error: error.message,
     });
   }
