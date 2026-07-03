ae9d38b8163c1fe91f5e8453dae0ca2d5916ae43
RappMicco <rapp.miccorizo@gmail.com>
2026-07-03 15:34:52 +0800

    feat: added helmet and cors

--- server/index.js
+++ server/index.js
@@ -3,6 +3,7 @@ import dotEnv from "dotenv";
 import cors from "cors";
 import cookieParser from "cookie-parser";
 import { connectDB } from "./configs/db.js";
+import { helmetConfig } from "./configs/helmetConfig.js";
 import employeeRoutes from "./routes/employeeRoutes.js";
 import adminRoutes from "./routes/adminRoutes.js";
 import summaryRoutes from "./routes/summaryRoutes.js";
@@ -16,6 +17,29 @@ connectDB();
 
 app.use(express.json());
 app.use(cookieParser());
+
+//update CORS to allow credentials and specify origin
+const allowedOrigins = [
+  process.env.CLIENT_URL,
+  process.env.CLIENT_URL_PROD,
+].filter(Boolean);
+
+const corsOptions = {
+  origin: (origin, callback) => {
+    if (!origin) return callback(null, true);
+
+    if (allowedOrigins.includes(origin)) {
+      return callback(null, true);
+    }
+
+    return callback(new Error(`CORS not allowed form origin: ${origin}`));
+  },
+  credentials: true,
+};
+
+app.use(helmetConfig);
+app.use(cors(corsOptions));
+// END of CORS UPDATE
 
 app.use("/api", employeeRoutes);
 app.use("/api/admin", adminRoutes);
