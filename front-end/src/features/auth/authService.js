b21be7695480d0e611ef3472d564db1b5e8d33a8
Rapp <rapp.miccorizo@gmail.com>
2026-07-05 22:33:58 +0800

    feat: added authThunk

--- front-end/src/features/auth/authService.js
+++ front-end/src/features/auth/authService.js
@@ -4,13 +4,17 @@ export const login = async (credentials) => {
   const response = await fetch(`${VITE_API_URL}/login`, {
     method: "POST",
     headers: {
-      "Content-type": "application/json",
+      "Content-Type": "application/json",
     },
     credentials: "include",
     body: JSON.stringify(credentials),
   });
 
+  const data = response.json();
+
   if (!response.ok) {
-    throw new Error("No data ");
+    throw new Error(data.message || "Login Failed.");
   }
+
+  return data;
 };
