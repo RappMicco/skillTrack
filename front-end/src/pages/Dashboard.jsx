e53bf05dd87c31dbe2eec78f5b2077948439b921
RappMicco <rapp.miccorizo@gmail.com>
2026-07-13 07:42:50 +0800

    feat: dashboard

--- front-end/src/pages/Dashboard.jsx
+++ front-end/src/pages/Dashboard.jsx
@@ -1,6 +1,6 @@
 export const Dashboard = () => {
   return (
-    <div className="flex bg-[linear-gradient(to_right,#001A31_50%,#2A2C8D_100%)] rounded-[20px] px-4 py-3">
+    <div className="flex bg-[linear-gradient(to_right,#001A31_30%,#2A2C8D_100%)] rounded-[20px] px-4 py-3">
       <h1 className="text-lg font-semibold text-[#E7EFF0]/51">
         Training Plan Overview Dashboard
       </h1>
