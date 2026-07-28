981bdf23ed62744fa568a4720bede36a548072c4
RAPP MICCO RIZO <rapp.miccorizo@gmail.com>
2026-07-28 09:09:02 +0000

    Front end

--- front-end/src/store/store.js
+++ front-end/src/store/store.js
@@ -1,10 +1,12 @@
 import { configureStore } from "@reduxjs/toolkit";
 import authReducer from "../features/auth/authSlice.js";
 import trainingReducer from "../features/statusSummary/statusSlice.js";
+import insightReducer from "../features/insights/insightSlice.js";
 
 export const store = configureStore({
   reducer: {
     auth: authReducer,
     training: trainingReducer,
+    insights: insightReducer,
   },
 });
