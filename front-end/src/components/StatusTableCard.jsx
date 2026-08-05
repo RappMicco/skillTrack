7327bd1ab55c6d9909259d6e81bc4c6776c92260
RAPP MICCO RIZO <rapp.miccorizo@gmail.com>
2026-08-05 00:42:11 +0000

    Front end

--- front-end/src/components/StatusTableCard.jsx
+++ front-end/src/components/StatusTableCard.jsx
@@ -1,13 +1,36 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { Search } from "lucide-react";
 import { statusCategory, statusHeader } from "../hook/statusCategory.js";
+import {
+  statusBgBorderColor,
+  initialIconBg,
+  viewIconColor,
+} from "../utils/StatusTableConfig.jsx";
+import { useSelector, useDispatch } from "react-redux";
+import { fetchUpcomingData } from "../features/statusTable/statusTableThunk.js";
 
 export const StatusTableCard = () => {
+  const { upcomingData } = useSelector((state) => state.statusData);
+  const dispatch = useDispatch();
   const [selectedStatus, setSelectedStatus] = useState("UPCOMING");
 
   const handleClickStatus = (item) => {
     setSelectedStatus(item);
   };
+
+  useEffect(() => {
+    const fetchStatusData = async () => {
+      try {
+        if (selectedStatus === "UPCOMING") {
+          await dispatch(fetchUpcomingData());
+        }
+      } catch (error) {
+        console.error(error);
+      }
+    };
+
+    fetchStatusData();
+  }, [selectedStatus, dispatch]);
   return (
     <>
       <div className="flex flex-col sm:flex-row sm:items-center justify-between gap-3 px-5 py-4 border-b border-[#06B6D4]/5">
@@ -63,22 +86,68 @@ export const StatusTableCard = () => {
 
           {/* ================================================================================= TABLE DETAILS =========================================================================================*/}
           <tbody>
-            <tr className="border-b border-white/4 hover:bg:white/3 transition-colors group">
-              <td className="px-5 py-4">
-                {/* initials and fullname container*/}
-                <div className="flex items-center gap-2.5">
-                  {/* initials */}
-                  <div className="w-7 h-7 rounded-full flex items-center justify-center text-[12px] font-bold text-white shrink-0 border border-cyan-500 bg-linear-to-br from-blue-500 to-cyan-400">
-                    R
-                  </div>
+            {upcomingData.map((item, index) => {
+              return (
+                <tr
+                  key={index}
+                  className="border-b border-white/4 hover:bg:white/3 transition-colors group"
+                >
+                  <td className="px-5 py-4">
+                    {/* initials and fullname container*/}
+                    <div className="flex items-center gap-2.5">
+                      {/* initials */}
+                      <div
+                        className={`w-7 h-7 rounded-full flex items-center justify-center text-[12px] font-bold text-white shrink-0 bg-linear-to-br ${initialIconBg(selectedStatus)}`}
+                      >
+                        R
+                      </div>
 
-                  {/* full name */}
-                  <span className="text-slate-300 font-medium text-xs whitespace-nowrap">
-                    Rapp Micco Rizo
-                  </span>
-                </div>
-              </td>
-            </tr>
+                      {/* full name */}
+                      <span className="text-slate-300 font-medium text-xs whitespace-nowrap">
+                        {item?.fullName}
+                      </span>
+                    </div>
+                  </td>
+                  {/* traininig name */}
+                  <td className="px-5 py-4 text-slate-400 text-xs">
+                    {item?.traingName}
+                  </td>
+                  {/*Institution */}
+                  <td className="px-5 py-4 text-slate-400 text-xs">TNSP</td>
+                  {/* Schedule */}
+                  <td className="px-5 py-4 text-slate-400 text-xs whitespace-nowrap">
+                    July 10 - July 18, 2026
+                  </td>
+                  {/* Status */}
+                  <td className="px-5 py-4">
+                    <span
+                      className={`inline-block px-2 py-0.5 rounded-full text-[10px] font-semibold uppercase tracking-wider ${statusBgBorderColor(selectedStatus)}`}
+                    >
+                      {selectedStatus}
+                    </span>
+                  </td>
+                  {/* Actions */}
+                  <td className="px-5 py-4">
+                    <div className="flex items-center gap-2">
+                      {/* view */}
+                      <button
+                        className={`text-[10px] font-semibold cursor-pointer ${viewIconColor(selectedStatus)}
+                                    px-4 py-1 rounded-lg transition-all duration-300 active:scale-95`}
+                      >
+                        View
+                      </button>
+                      {/* edit */}
+                      <button
+                        className="text-[10px] font-semibold text-slate-500 hover:text-slate-300 border border-white/10 hover:border-white/20 px-4 py-1 rounded-lg transition-all
+                                    duration-300 active:scale-95 cursor-pointer"
+                      >
+                        Edit
+                      </button>
+                    </div>
+                  </td>
+                </tr>
+              );
+            })}
           </tbody>
         </table>
       </div>
