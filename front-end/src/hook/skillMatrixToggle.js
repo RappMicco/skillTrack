f726617739861b78d8428d9f4bce5437330e02c6
RAPP MICCO RIZO <rapp.miccorizo@gmail.com>
2026-08-19 13:30:22 +0000

    Front end

---
+++ front-end/src/hook/skillMatrixToggle.js
@@ -0,0 +1,26 @@
+export const skillMatrixToggle = [
+  {
+    id: 1,
+    label: "All",
+  },
+  {
+    id: 2,
+    label: "Management & Delivery Skill",
+  },
+  {
+    id: 3,
+    label: "Softskill",
+  },
+  {
+    id: 4,
+    label: "Technical Skill",
+  },
+  {
+    id: 5,
+    label: "Application Skill",
+  },
+  {
+    id: 6,
+    label: "Deployment Skill",
+  },
+];
