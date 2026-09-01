72b34c091778585ef3208ec5062d757173df2fe6
RAPP MICCO RIZO <rapp.miccorizo@gmail.com>
2026-09-01 21:40:09 +0000

    Front end

--- front-end/src/features/maintenance/maintenanceSlice.js
+++ front-end/src/features/maintenance/maintenanceSlice.js
@@ -50,11 +50,7 @@ const maintenanceSlice = createSlice({
       state.error = null;
     },
     clearMaintenanceMessage: (state) => {
-<<<<<<< HEAD
       state.message = null;
-=======
-      state.message = "";
->>>>>>> e39386fe6dcf2acb5c54b4c0dec66677f614a99c
     },
   },
   extraReducers: (builder) => {
